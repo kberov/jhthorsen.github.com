4666cbffb031d12cccdf18c475afdb78e564a3f2
Jan Henning Thorsen <jhthorsen@cpan.org>
2013-05-02 21:37:32 +0200

    Add /contact

--- lib/Batware.pm
+++ lib/Batware.pm
@@ -35,9 +35,35 @@ Called to set up the routes.
 
 sub startup {
   my $self = shift;
+  my $config = $self->plugin('config');
   my $r = $self->routes;
 
+  $self->plugin(Mail => $config->{Mail});
+
   $r->get('/')->to(template => 'index');
+  $r->get('/contact')->to(template => 'contact', report => '')->name('contact');
+  $r->post('/contact')->to(cb => \&_post_contat_form);
+}
+
+sub _post_contat_form {
+  my $c = shift;
+
+  eval {
+    $c->param('message') or die 'No message?';
+    $c->mail(
+      to => $c->app->config->{Mail}{receiver},
+      from => $c->param('email') || die('Your email is missing.'),
+      subject => $c->param('subject') || die('Subject need to be filled out.'),
+      template => 'contact',
+      format => 'mail',
+    );
+    $c->stash(report => 'Message was sent!');
+  } or do {
+    my $e = $@;
+    $c->app->log->error($e);
+    $e =~ s/ at \S+.*//s;
+    $c->stash(report => $e || 'Could not send message!');
+  };
 }
 
 =head1 AUTHOR
